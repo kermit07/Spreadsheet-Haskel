31e36666ce561bfc137691d02a7cfadf89068885
kermit07 <abckermit@gmail.com>
2017-01-08 16:47:01 +0100

    Aktualizacja komórek przy usuwaniu wiersza lub kolumny
    Przy usunięciu wiersza lub kolumny usunięcie ze wszystkich funkcji (add, mul, avg) wszystkich wystąpień usuwanych komórek
    Powrót na (Int, Int) w Location. Char będzie tylko brany w UI i interpretowany dalej jako Int

--- Spreadsheet.hs
+++ Spreadsheet.hs
@@ -3,7 +3,7 @@ import Data.Function
 import Data.Maybe
 import Control.Applicative
 
-type Location = (Char, Int)
+type Location = (Int, Int)
 
 data Cell = Empty
           | Num Int
@@ -27,8 +27,8 @@ type Spreadsheet = [[Cell]]
 
 ss :: Spreadsheet
 ss = [[Num 2, Text "abc", Num 4],
-      [Mul [('A', 1), ('C', 1)] (Just 99.0), Add [('A', 1), ('C', 1)] (Just 99.0), Add [('A', 1), ('C', 1), ('C', 1)] (Just 99.0)],
-      [Avg [('A', 1), ('A', 2)] (Just 2.0), Empty, Empty]]
+      [Mul [(1, 1), (3, 1)] (Just 99.0), Add [(1, 1), (3, 1)] (Just 99.0), Add [(1, 1), (3, 1), (3, 1)] (Just 99.0)],
+      [Avg [(1, 1), (1, 2)] (Just 2.0), Mul [(1, 3), (1, 3)] (Just 99.0), Empty]]
 
 updateCell :: Spreadsheet -> Cell -> Cell
 updateCell s (Empty) = Empty
@@ -80,14 +80,40 @@ addCol s = map (++ [Empty]) s
 remRow :: Spreadsheet -> Spreadsheet
 remRow s = case length s of
                 0 -> s
-                _ -> take (length s-1) s
+                _ -> take rowIndex $ updateAll $ validateRow s rowIndex
+                     where rowIndex = length s - 1
 
 remCol :: Spreadsheet -> Spreadsheet
 remCol s = case length s of
                 0 -> s
                 _ -> case length (s !! 0) of
                           0 -> s
-                          _ -> map (take (length (s !! 0) - 1)) s
+                          _ -> map (take colIndex) $ updateAll $ validateCol s colIndex
+                               where colIndex = length (s !! 0) - 1
+
+-- removes all elements from [Locations] that contains colIndex == cx OR rowIndex == rx
+remLocations :: [Location] -> Int -> Int -> [Location]
+remLocations [] _ _ = []
+remLocations ((c, r):ls) cx rx | (mkIndex c == cx || mkIndex r == rx) = remLocations ls cx rx
+                               | otherwise = (c, r) : remLocations ls cx rx
+
+-- validates Cell when remove col or row
+validateCell :: Spreadsheet -> Int -> Int -> Cell -> Cell
+validateCell s cx rx (Add l _) = Add newL (doSum newL s)
+                where newL = remLocations l cx rx
+validateCell s cx rx (Mul l _) = Mul newL (doMul newL s)
+                where newL = remLocations l cx rx
+validateCell s cx rx (Avg l _) = Avg newL (doAvg newL s)
+                where newL = remLocations l cx rx
+validateCell _ _ _ cell = cell
+
+-- returns Spreadsheet without Location that's col index references to cx
+validateCol :: Spreadsheet -> Int -> Spreadsheet
+validateCol s cx = map (map (validateCell s cx (-1))) s
+
+-- returns Spreadsheet without Locations that's row index references to rx
+validateRow :: Spreadsheet -> Int -> Spreadsheet
+validateRow s rx = map (map (validateCell s (-1) rx)) s
 
 getCell :: Spreadsheet -> Location -> Maybe Cell
 getCell s (c, r) | 0 < length s && 
@@ -95,8 +121,8 @@ getCell s (c, r) | 0 < length s &&
                    row < length s &&
                    col < length (s !! 0) = Just (s !! row !! col)
                  | otherwise = Nothing
-                       where col = mkColIndex c
-                             row = mkRowIndex r
+                       where col = mkIndex c
+                             row = mkIndex r
 
 setCell :: Spreadsheet -> Location -> Cell -> Spreadsheet
 setCell s (c, r) cell =
@@ -104,8 +130,8 @@ setCell s (c, r) cell =
                        take row s ++
                        [take col (s !! row) ++ [cell] ++ drop (col + 1) (s !! row)] ++
                        drop (row + 1) s
-                       where col = mkColIndex c
-                             row = mkRowIndex r
+                       where col = mkIndex c
+                             row = mkIndex r
 
 setEmpty :: Spreadsheet  -> Location -> Spreadsheet
 setEmpty s l = setCell s l Empty
@@ -125,15 +151,15 @@ setMul s ll l = setCell s l (Mul ll Nothing)
 setAvg :: Spreadsheet -> [Location] -> Location -> Spreadsheet
 setAvg s ll l = setCell s l (Avg ll Nothing)
 
-mkColIndex :: Char -> Int
-mkColIndex c | fromEnum c >= 65 &&
-             fromEnum c <= 90 = fromEnum c - 65
-           | fromEnum c >= 97 &&
-             fromEnum c <= 122 = fromEnum c - 97
-           | otherwise = error "Indeksacja od A do Z!"
+charToIntIndex :: Char -> Int
+charToIntIndex c | fromEnum c >= 65 &&
+                   fromEnum c <= 90 = fromEnum c - 65
+                 | fromEnum c >= 97 &&
+                   fromEnum c <= 122 = fromEnum c - 97
+                 | otherwise = error "Indeksacja od A do Z!"
 
-mkRowIndex :: Int -> Int
-mkRowIndex i = case (i < 1) of
+mkIndex :: Int -> Int
+mkIndex i = case (i < 1) of
                  True  -> error "Indeksacja od 1!"
                  False -> i-1
 

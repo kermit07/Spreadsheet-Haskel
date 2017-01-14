bcff8fdb38aec0c162c5fdcfc388047d1c93286e
kermit07 <abckermit@gmail.com>
2017-01-14 23:58:23 +0100

    Parser na podstawie tego co na zajęciach robiliśmy
    Dodałem swoją funkcję myParser, która zwraca myResult czyli wszystkie możliwe opcjie

--- Parser.hs
+++ Parser.hs
@@ -43,8 +43,17 @@ sat p = do x <- item
 digit :: Parser Char
 digit = sat isDigit
 
+letter :: Parser Char
+letter = sat isLetter
+
 char :: Char -> Parser Char
 char x = sat (== x)
+
+char2 :: String -> Parser Char
+char2 [] = failure
+char2 (x:xs) = sat (== x)
+              +++
+               char2 xs
 
 many :: Parser a -> Parser [a]
 many p = many1 p +++ return []
@@ -54,20 +63,77 @@ many1 p = do v <- p
              vs <- many p
              return (v:vs)
 
+type Loc = (Int, Int)
+
 -- w Parser.hs niestety nie można zaimportować Spreadsheet.hs, bo cykle importów nie są dozwolone
 -- dlatego trzeba stworzyć jakąś kolejną strukturę i sprytnie w Main.hs ją przerabiać na typ Cell
-data MyResult = Num Int
-              | Text String
+data MyResult = ParserNum Int
+              | ParserText String
+              | ParserSum [Loc]
+              | ParserMul [Loc]
+              | ParserAvg [Loc]
 
 instance Show MyResult where
-  show (Num a) = show a
-  show (Text a) = a
+  show (ParserNum a) = "NUM " ++ show a
+  show (ParserText a) = "TEXT " ++ a
+  show (ParserSum x) = "SUM " ++ show x
+  show (ParserMul x) = "MUL " ++ show x
+  show (ParserAvg x) = "AVG " ++ show x
+
+parseInt :: Parser MyResult
+parseInt = do d <- many1 digit
+              return $ ParserNum (read d :: Int)
+
+parseText :: Parser MyResult
+parseText = do d <- many1 item
+               return $ ParserText d
+
+parseLoc :: Parser Loc
+parseLoc = do c <- letter
+              d <- many1 digit
+              return $ ((charToIntIndex c), (read d :: Int))
+
+parseLoc2 :: Parser Loc
+parseLoc2 = do char ' '
+               c <- letter
+               d <- many1 digit
+               return $ ((charToIntIndex c), (read d :: Int))
+
+parseSum :: Parser MyResult
+parseSum = do char2 "sS"
+              char2 "uU"
+              char2 "mM"
+              e <- many1 parseLoc2
+              return $ ParserSum e
+
+parseMul :: Parser MyResult
+parseMul = do char2 "mM"
+              char2 "uU"
+              char2 "lL"
+              e <- many1 parseLoc2
+              return $ ParserMul e
+
+parseAvg :: Parser MyResult
+parseAvg = do char2 "aA"
+              char2 "vV"
+              char2 "gG"
+              e <- many1 parseLoc2
+              return $ ParserAvg e
 
 myParser :: Parser MyResult -- tutaj zacząłem robić swój parser. Dla testów na razie
-myParser = do d <- many1 digit
-              return $ Num (read d :: Int)
+myParser = do parseSum
              +++
-           do char '('
-              e <- digit
-              char ')'
-              return $ Num (digitToInt e)
+              parseMul
+             +++
+              parseAvg
+             +++
+              parseInt
+             +++
+              parseText
+
+charToIntIndex :: Char -> Int
+charToIntIndex c | fromEnum c >= 65 &&
+                   fromEnum c <= 90 = fromEnum c - 64
+                 | fromEnum c >= 97 &&
+                   fromEnum c <= 122 = fromEnum c - 96
+                 | otherwise = error "Indeksacja od A do Z!"
